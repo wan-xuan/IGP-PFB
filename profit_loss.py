fdc67c52d7c2233bb68ede77d45490ca74f4adc6
wan-xuan <milkuyu1230@gmail.com>
2024-02-01 17:40:54 +0800

    update on deficit

--- profit_loss.py
+++ profit_loss.py
@@ -26,12 +26,14 @@ def analyze_profit_n_loss(PnL):
                 profit_n_loss_changesday = PnL[i+1][0]  # Use the day of the second data point
         else:
             decrease += 1
-            print(f"[NET PROFIT DEFICIT] DAY: {PnL[i][0]+1}, AMOUNT: SGD{abs(PnL[i][1] - PnL[i+1][1])}")
-            deficits.append((abs(PnL[i][1] - PnL[i+1][1]), PnL[i][0]+1))
-            
-            if (PnL[i][1] - PnL[i+1][1]) < profit_n_loss_changes:
+            if (PnL[i][1] - PnL[i+1][1]) > profit_n_loss_changes:
                 profit_n_loss_changes = (PnL[i][1] - PnL[i+1][1])
                 profit_n_loss_changesday = PnL[i][0] + 1
+            
+            elif increase >0 and decrease >0:
+                print(f"[NET PROFIT DEFICIT] DAY: {PnL[i][0]+1}, AMOUNT: SGD{abs(PnL[i][1] - PnL[i+1][1])}")
+                deficits.append((abs(PnL[i][1] - PnL[i+1][1]), PnL[i][0]+1))
+        
 
     if increase > 0 and decrease == 0:
         return f"[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\
@@ -41,6 +43,7 @@ def analyze_profit_n_loss(PnL):
             \n[HIGHEST PROFIT DEFICIT] DAY:{profit_n_loss_changesday}, AMOUNT: SGD{profit_n_loss_changes}"
     else:
         deficits.sort(reverse=True)
-        return f"[HIGHEST NET PROFIT DEFICIT] DAY:{deficits[0][1]}, AMOUNT: SGD{abs(deficits[0][0])}\
-                \n[2ND  HIGHEST NET PROFIT DEFICIT] DAY:{deficits[1][1]}, AMOUNT: SGD{abs(deficits[1][0])}\
-                \n[3RD  HIGHEST NET PROFIT DEFICIT] DAY:{deficits[2][1]}, AMOUNT: SGD{abs(deficits[1][0])}"
+        
+    return f"[HIGHEST NET PROFIT DEFICIT] DAY:{deficits[0][1]}, AMOUNT: SGD{abs(deficits[0][0])}\
+        \n[2ND  HIGHEST NET PROFIT DEFICIT] DAY:{deficits[1][1]}, AMOUNT: SGD{abs(deficits[1][0])}\
+        \n[3RD  HIGHEST NET PROFIT DEFICIT] DAY:{deficits[2][1]}, AMOUNT: SGD{abs(deficits[1][0])}"
