b3afef7e1755c4c304b74706ff74926c790efeea
wan-xuan <milkuyu1230@gmail.com>
2024-02-02 03:24:19 +0800

    comment added

--- overheads.py
+++ overheads.py
@@ -7,17 +7,21 @@ def read_overheads(file_path):
         next(reader)  # Skip the header
 
         overHead = []
-        highest = 0
 
         for row in reader:
-            expense_type = row[0] #type of expenses
-            amount = float(row[1]) #percentage of each type out of 100%
+            #type of expenses
+            expense_type = row[0]
+            #percentage of each type out of 100%, with decimals
+            amount = float(row[1])
+            #allocate the names to the rows 
             overHead.append((expense_type, amount))
 
         return overHead
 
 def analyze_highest_overhead(overheads):
+    #viarable starting at 0, coded to store the highest number comparing to the previous loop
     highest_amount = 0
+    # str to be filled in by expense_type
     highest_type = ""
 
     for expense_type, amount in overheads:
@@ -25,5 +29,5 @@ def analyze_highest_overhead(overheads):
             highest_amount = amount
             highest_type = expense_type
 
+    #returns the output for all kind of scenarios
     return f"[HIGHEST OVERHEAD] {highest_type}:{highest_amount}%"
-
