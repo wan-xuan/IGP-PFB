1795c833585249e2798288ff55b75c58e1ecfa43
Snakeeyes090406 <ashapril2006901@gmail.com>
2024-02-01 16:23:56 +0800

    main file updated

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import profit_loss
 import overheads
 
 # Define file paths
-fp = Path.cwd() / "surplus.csv" #cash-on-hand-sgd.csv
-np = Path.cwd() / "deficit.csv" #profit-and-loss-sgd.csv
+fp = Path.cwd() / "cash-on-hand-sgd.csv" #cash-on-hand-sgd.csv
+np = Path.cwd() / "profit-and-loss-sgd.csv" #profit-and-loss-sgd.csv
 qp = Path.cwd() / "overheads-day-90.csv" #overheads-day-90.csv
 
 # Use functions from the imported modules
@@ -18,6 +18,18 @@ if overheads_data:
     print(highest_overhead_result)
 else:
     print("No data found in the Overheads file")
+
+if cash_on_hand_data:
+    cash_on_hand_result = coh.analyze_cash_on_hand(cash_on_hand_data)
+    print(cash_on_hand_result)
+else:
+    print("No data found in the Cash-On-Hand file")
+
+if profit_loss_data:
+    profit_loss_result = profit_loss.analyze_profit_n_loss(profit_loss_data) # Fix: Use profit_loss instead of profit_loss2
+    print(profit_loss_result)
+else:
+    print("No data found in the Profit and Loss file")
 
 
 
