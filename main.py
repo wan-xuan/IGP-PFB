e5e27a3c2ee3c42a316b2aa4005e0988c954f178
wan-xuan <milkuyu1230@gmail.com>
2024-02-02 19:40:38 +0800

    most recent update with comments

--- main.py
+++ main.py
@@ -15,21 +15,21 @@ overheads_data = overheads.read_overheads(qp)
 
 # uses the function imported from the modules
 if overheads_data:
-    highest_overhead_result = overheads.analyze_highest_overhead(overheads_data)
+    highest_overhead_result = overheads.analyse_highest_overhead(overheads_data)
     print(highest_overhead_result)  
 else: #lets you know if dataset provided is invalid
     print("No data found in the Overheads file") 
 
 # uses the function imported from the modules
 if cash_on_hand_data:
-    cash_on_hand_result = coh.analyze_cash_on_hand(cash_on_hand_data)
+    cash_on_hand_result = coh.analyse_cash_on_hand(cash_on_hand_data)
     print(cash_on_hand_result)
 else: #lets you know if dataset provided is invalid
     print("No data found in the Cash-On-Hand file")
 
 # uses the function imported from the modules
 if profit_loss_data:
-    profit_loss_result = profit_loss.analyze_profit_n_loss(profit_loss_data) 
+    profit_loss_result = profit_loss.analyse_profit_n_loss(profit_loss_data) 
     print(profit_loss_result)
 else: #lets you know if dataset provided is invalid
     print("No data found in the Profit and Loss file") 
