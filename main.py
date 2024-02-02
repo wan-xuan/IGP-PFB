ef04e3a3cc60d9e6c2ed046fac3b5e041460bead
wan-xuan <milkuyu1230@gmail.com>
2024-02-02 14:45:35 +0800

    updated

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import profit_loss
 import overheads
 
 # Defines file path
-fp = Path.cwd() / "cash-on-hand-sgd.csv"
-np = Path.cwd() / "profit-and-loss-sgd.csv" 
-qp = Path.cwd() / "overheads-day-90.csv" 
+fp = Path.cwd() / "cash_on_hand.csv"
+np = Path.cwd() / "profit_and_loss.csv" 
+qp = Path.cwd() / "overheads.csv" 
 
 #imports data to its variable
 cash_on_hand_data = coh.read_csv(fp)
@@ -20,16 +20,18 @@ if overheads_data:
 else: #lets you know if dataset provided is invalid
     print("No data found in the Overheads file") 
 
+# uses the function imported from the modules
 if cash_on_hand_data:
     cash_on_hand_result = coh.analyze_cash_on_hand(cash_on_hand_data)
     print(cash_on_hand_result)
-else:
+else: #lets you know if dataset provided is invalid
     print("No data found in the Cash-On-Hand file")
 
+# uses the function imported from the modules
 if profit_loss_data:
     profit_loss_result = profit_loss.analyze_profit_n_loss(profit_loss_data) 
     print(profit_loss_result)
-else:
+else: #lets you know if dataset provided is invalid
     print("No data found in the Profit and Loss file") 
 # ends at the last module imported----Profit and Loss
 
