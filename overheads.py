4f4d8e3da18974696ec3115aa7fe226109316ebe
wan-xuan <milkuyu1230@gmail.com>
2024-02-02 19:40:48 +0800

    most recent update with comments

--- overheads.py
+++ overheads.py
@@ -1,25 +1,28 @@
 from pathlib import Path
 import csv
 
+#this function reads the CSV file
 def read_overheads(file_path):
     with file_path.open(mode='r', encoding='UTF-8', newline="") as file:
         reader = csv.reader(file)
         next(reader)  # Skip the header
 
+        #creates an empty list to store data
         overHead = []
 
         for row in reader:
             #type of expenses
             expense_type = row[0]
-            #percentage of each type out of 100%, with decimals
+            #percentage of each type out of 100%, as a decimal
             amount = float(row[1])
-            #allocate the names to the rows 
+            #allocate the names(variable) to the rows 
             overHead.append((expense_type, amount))
 
         return overHead
 
-def analyze_highest_overhead(overheads):
-    #viarable starting at 0, coded to store the highest number comparing to the previous loop
+#this function analyses the list of tuples representing the overhead data
+def analyse_highest_overhead(overheads):
+    #variable starting at 0, coded to store the highest number comparing to that of the previous loop
     highest_amount = 0
     # str to be filled in by expense_type
     highest_type = ""
